c1c77aa5f9b032950e2a27b9de7f0fd4a76ff789
Saulo Mairesse <saulodavi2001@gmail.com>
2022-02-14 17:02:05 -0300

    Versao para envio

--- scrypt.js
+++ scrypt.js
@@ -1,44 +1,47 @@
 let dadoNome;
+
 loginUsuario();
 
 function loginUsuario(){
     let requestName = prompt('Digite seu Nick: ');
     let nome = {
-        name: `${requestName}`
+        name: requestName
     }
-    dadoNome = `${requestName}`;
+    dadoNome = requestName;
+    console.log(dadoNome)
     if (requestName != null){        
         let promessaLoginUser = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants',{name: `${requestName}`});
         promessaLoginUser.catch(tratarErro);
-        promessaLoginUser.then(setInterval(requestMensage,3000));
-        //setInterval(manterUsuarioON(nome),5000)
-        setInterval(function() {manterUsuarioON(nome); }, 5000);
+        promessaLoginUser.then(setInterval(requestMessage,3000)); //solicitar mensagens no servidor
+        setInterval(function() {keepUserLoggedIn(nome); }, 5000); //manter usuario logado
     }
 }   
 
-function requestMensage(){
+function requestMessage(){ //solicitar mensagens do servidor
     let promessaMensagensDoServidor = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
-    promessaMensagensDoServidor.then(mostrarConsoleEntrada);
+    promessaMensagensDoServidor.then(printMessage);
 }
 
-function mostrarConsoleEntrada(resposta){
+function printMessage(resposta){
     let mainHTML = document.querySelector('main');
     mainHTML.innerHTML = '';
     for(let i = 0; i < resposta.data.length; i++){
+        if(resposta.data[i].to == dadoNome || resposta.data[i].to == 'Todos'){
             mainHTML.innerHTML += `<section>
                                         <ul>
-                                            <li><b>${resposta.data[i].time}</b> <b><strong>${resposta.data[i].from}</strong></b> para <b>${resposta.data[i].to}</b>: ${resposta.data[i].text}</li>
-                                            <li>${resposta.data[i].type}</li>
+                                            <li data-identifier="message"><b class="hora">(${resposta.data[i].time})</b> <b class="remetente">${resposta.data[i].from}</b> para <b class="destinatario">${resposta.data[i].to}</b>: ${resposta.data[i].text}</li>
                                         </ul>
                                     </section>`;
-    }
-    console.log('Mostrando')
+            if(resposta.data[i].type == "private_message"){
+                document.querySelector('main').lastChild.classList.add('private-message')
+                }
+        }
     mainHTML.lastChild.scrollIntoView();
+    }
 }
 
-function manterUsuarioON(nome){
-    let usuarioOn = axios.post('https://mock-api.driven.com.br/api/v4/uol/status',nome);
-    usuarioOn.then(console.log('Estou online'));
+function keepUserLoggedIn(nome){
+    let userLogged = axios.post('https://mock-api.driven.com.br/api/v4/uol/status',nome);
 }
 
 function tratarErro(erro) {
@@ -48,10 +51,17 @@ function tratarErro(erro) {
     loginUsuario();
 }
 
-function enviar() {
-    let mensagemInput = document.querySelector('.mensagem').value; 
-    const objeto = {from: `${dadoNome}`, to: "Todos", text: `${mensagemInput}`, type: "message"};
-    let enviarMensagem = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages',objeto);
-    enviarMensagem.then(console.log(mensagemInput));
-    enviarMensagem.catch(alert('Deu ruim'));
+function sendMessage() {
+    let mensagemInput = document.querySelector('.mensagem').value;
+    if(mensagemInput != ''){
+        const objeto = {from: dadoNome, to: "Todos", text: mensagemInput, type: "message"};
+        let enviarMensagem = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages',objeto);
+        enviarMensagem.then(requestMessage);
+        enviarMensagem.catch(qualquerFunc);
+    }
+}        
+
+function qualquerFunc() {
+    alert('Deu ruim');
+    window.location.reload();
 }
